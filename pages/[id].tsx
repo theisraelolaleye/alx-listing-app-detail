779b254e8ada9dac2352904519f921d8ccda3c22
theisraelolaleye <theisraelolaleye@gmail.com>
2025-10-05 01:32:55 +0100

    Review section, booking section and Property Details Page

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,6 +1,6 @@
 import { PROPERTYLISTINGSAMPLE } from "@/constants/index";
 import { useRouter } from "next/router";
-import PropertyDetail from "@/pages/PropertyDetail";
+import PropertyDetail from "@/components/property/PropertyDetail";
 
 export default function PropertyPage() {
   const router = useRouter();
